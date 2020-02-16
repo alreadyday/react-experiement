d5eadefa8aa0fd4213e977f5ea31c79a3251fd3d
franke <alreadyday@gmail.com>
2020-02-17 00:17:08 +0800

    add coverage test

--- test/specs/main/categories.js
+++ test/specs/main/categories.js
@@ -1,11 +1,32 @@
 describe("Category Search", function() {
   beforeAll(() => {
+    /**
+     * enable necessary domains
+     */
+    browser.cdp("Profiler", "enable");
+    browser.cdp("Debugger", "enable");
+
+    /**
+     * start test coverage profiler
+     */
+    browser.cdp("Profiler", "startPreciseCoverage", {
+      callCount: true,
+      detailed: true
+    });
     browser.url("http://localhost:3000");
   });
 
   it("searches cateogory session", function() {
     const categorySession = $(".category-session li");
-    console.warn(categorySession.length);
     categorySession.waitForExist(5000);
+  });
+
+  afterAll(() => {
+    /**
+     * capture test coverage
+     */
+    const { result } = browser.cdp("Profiler", "takePreciseCoverage");
+    const coverage = result.filter(res => res.url !== "");
+    console.log(coverage);
   });
 });
